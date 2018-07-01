ef4ec3d4b2d8c6e557f4950ed1169c879b6973a2
Diogo Silva <diogo@diogok.net>
2018-06-30 22:44:07 -0300

    Add consul recursor DNS to host resolv.conf nameserver entry

--- scripts/31-consul-service.sh
+++ scripts/31-consul-service.sh
@@ -11,6 +11,9 @@ sudo mv /etc/ops/consul.service /etc/systemd/system/consul.service
 echo CONSUL_UI_BETA=true >> /etc/ops/env
 echo '{"connect":{"enabled":true}}' > /etc/ops/consul.json
 
+NAMESERVER=`cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }' | head -n1`
+echo NAMESERVER=$NAMESERVER >> /etc/ops/env
+
 if [ "$ROLE" == "manager" ]; then
   echo CONSUL_ROLE_FLAGS=\"-server -bootstrap-expect=1\" >> /etc/ops/env
 fi
