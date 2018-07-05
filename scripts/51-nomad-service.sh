9ea0b62cd79788bf113df56a5a23bf615da00117
Diogo Souza da Silva <diogo.souza@olxbr.com>
2018-07-05 17:01:20 -0300

    Enable docker privileged and raw_exec on nomad clients

--- scripts/51-nomad-service.sh
+++ scripts/51-nomad-service.sh
@@ -6,12 +6,14 @@ nomad status && exit 0
 
 . /etc/ops/env # load config
 
+echo 'client { options { docker.privileged.enabled = "true" "driver.raw_exec.enable" = "1"} }' > /etc/ops/nomad.hcl
+
 if [ "$ROLE" == "manager" ]; then
   echo NOMAD_ROLE_FLAGS=\"-server -bootstrap-expect=1\" >> /etc/ops/env
 fi
 
 if [ "$ROLE" != "manager" ]; then
-  echo NOMAD_ROLE_FLAGS=-client >> /etc/ops/env
+  echo NOMAD_ROLE_FLAGS=-client -config=/etc/ops/nomad.hcl >> /etc/ops/env
 fi
 
 sudo mv /etc/ops/nomad.service /etc/systemd/system/nomad.service
